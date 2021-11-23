0b23ac95442d8a7f701507d085be4eb725f5aa32
MartinThibaud <martin.thibaud@dbmail.com>
2021-11-23 08:14:54 +0100

    Déplacement de XMLParserTest.java dans le dossier de test

--- src/test/java/Util/XMLParserTest.java
+++ src/test/java/Util/XMLParserTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class XMLParserTest {
 
