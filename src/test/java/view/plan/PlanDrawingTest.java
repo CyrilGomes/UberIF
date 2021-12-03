e7405cce2b83f7e6e04191e93213f5f01a13f07a
MartinThibaud <martin.thibaud@dbmail.com>
2021-12-03 11:37:45 +0100

    Ajout des tests unitaires pour PlanDrawing.java

---
+++ src/test/java/view/plan/PlanDrawingTest.java
@@ -0,0 +1,14 @@
+package view.plan;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PlanDrawingTest extends TestCase {
+	@Test
+	public void testGetCoordinate() {
+	}
+
+	@Test
+	public void testGetCoordinateY() {
+	}
+}
